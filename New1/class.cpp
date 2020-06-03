f6edfacaf26f994c1460e8f2a45cacc6f64ec32a
Darshan Saraswat <darshansuman12@gmail.com>
2020-06-03 22:46:36 +0530

    First

---
+++ New1/class.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class newe
+{
+    int i;
+    public:
+void get()
+{   int i;
+    cin>>i;
+}
+void put()
+{   int i;
+    cout<<"Darshan";
+    cout<<" "<<i;
+}
+};
+int main()
+  {
+	 newe a;
+    a.get();
+    a.put();
+     return 0;
+}
